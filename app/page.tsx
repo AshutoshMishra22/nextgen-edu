f2d78b186f26b6d110e959c7b298dc09d67a400b
AshutoshMishra22 <ashutoshmishraofficial22@gmail.com>
2024-10-30 22:28:02 +0530

    hero banner added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+import { Carousel } from "antd";
 import { NavigationHeader } from "./component";
+import styles from "./page.module.scss";
+import { hero_banner_list } from "./utils/constant";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <NavigationHeader />
-      <div style={{ height: 1000 }}>TEST</div>
+      <Carousel arrows>
+        {hero_banner_list.map((img, index) => (
+          <Image
+            src={img}
+            alt={`banner-${index}`}
+            className={styles.carousel_image}
+          />
+        ))}
+      </Carousel>
+      <section className={styles.page_children_container}></section>
     </main>
   );
 }
--- app/utils/constant.ts
+++ app/utils/constant.ts
@@ -1,1 +1,4 @@
-const NAVIGATION_CONTROL_LIST = ["Home", "Admission"];
+import school1 from "../assest/school_1.svg";
+const hero_banner_list = [school1, school1];
+
+export { hero_banner_list };
