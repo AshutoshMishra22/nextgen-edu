2c3aa4086e0c22a3a194b754ffd0b1c951fcca4b
AshutoshMishra22 <ashutoshmishraofficial22@gmail.com>
2024-10-30 22:52:23 +0530

    header modified

--- app/component/Navigation/index.tsx
+++ app/component/Navigation/index.tsx
@@ -1,8 +1,7 @@
 import React, { FC } from "react";
 import { PropTypes } from "./propTypes";
-import { Flex, Space } from "antd";
+import { Flex, Menu } from "antd";
 import styles from "./index.module.scss";
-import CDropdown from "../CDropdown";
 import { navigationControlList } from "./helper";
 
 const NavigationHeader: FC = ({}: PropTypes) => {
@@ -13,18 +12,11 @@ const NavigationHeader: FC = ({}: PropTypes) => {
       className={styles.navigation_container}
     >
       <span> ICON </span>
-      <Flex gap={16} className={styles.navigation_control}>
-        {navigationControlList.map((obj) => {
-          if (obj.menuItem) {
-            return (
-              <CDropdown items={obj.menuItem} key={obj.label}>
-                {obj.label}
-              </CDropdown>
-            );
-          }
-          return <Space key={obj.label}>{obj.label}</Space>;
-        })}
-      </Flex>
+      <Menu
+        mode="horizontal"
+        items={navigationControlList}
+        className={styles.navigation_control}
+      />
     </Flex>
   );
 };
