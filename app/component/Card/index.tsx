9eafc83265428fceee30667cbddbee69a22ba87e
AshutoshMishra22 <ashutoshmishraofficial22@gmail.com>
2024-11-01 19:55:07 +0530

    css fix

--- app/component/Card/index.tsx
+++ app/component/Card/index.tsx
@@ -4,18 +4,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBullseye } from "@fortawesome/free-solid-svg-icons";
 import { actionHighlight } from "@/app/utils/constant";
 import styles from "./index.module.scss";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
-const SubCard = ({ item }: { item: Record<string, string> }) => {
+const SubCard = ({ item }: { item: Record<string, string | IconProp> }) => {
   return (
     <Card hoverable>
-      <FontAwesomeIcon icon={faBullseye} className={styles.card_icon} />
-      <div className={styles.card_title}>{item.title}</div>
+      <FontAwesomeIcon
+        icon={item.icon as IconProp}
+        className={styles.card_icon}
+      />
+      <div className={styles.card_title}>{item.title as string}</div>
     </Card>
   );
 };
 const CCard: React.FC = () => (
   <Flex gap={16} className={styles.card_group}>
-    {actionHighlight.map((item: Record<string, string>) => (
+    {actionHighlight.map((item: Record<string, string | IconProp>) => (
       <SubCard item={item} />
     ))}
   </Flex>
