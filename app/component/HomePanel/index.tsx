6be937079c4013b78bf3e85f17d79ecdede26350
AshutoshMishra22 <ashutoshmishraofficial22@gmail.com>
2024-11-01 21:35:24 +0530

    info added

--- app/component/HomePanel/index.tsx
+++ app/component/HomePanel/index.tsx
@@ -1,80 +1,51 @@
-import { Flex } from "antd";
+import { Avatar, Card, Flex, Timeline } from "antd";
 import React, { FC } from "react";
 import styles from "./index.module.scss";
 import {
   BankOutlined,
-  EditOutlined,
   ReconciliationOutlined,
+  SmileOutlined,
 } from "@ant-design/icons";
 import { schoolMessage } from "@/app/utils/constant";
 import { HomePanelProptype } from "./proptypes";
-const dataList = [
-  {
-    gender: "female",
-    name: {
-      title: "Miss",
-      first: "Jade",
-      last: "Gill",
-    },
-    email: "jade.gill@example.com",
-    picture: {
-      large: "https://randomuser.me/api/portraits/women/66.jpg",
-      medium: "https://randomuser.me/api/portraits/med/women/66.jpg",
-      thumbnail: "https://randomuser.me/api/portraits/thumb/women/66.jpg",
-    },
-    nat: "CA",
-  },
-  {
-    gender: "male",
-    name: {
-      title: "Mr",
-      first: "Zvezdan",
-      last: "Vujičić",
-    },
-    email: "zvezdan.vujicic@example.com",
-    picture: {
-      large: "https://randomuser.me/api/portraits/men/40.jpg",
-      medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
-      thumbnail: "https://randomuser.me/api/portraits/thumb/men/40.jpg",
-    },
-    nat: "RS",
-  },
-  {
-    gender: "male",
-    name: {
-      title: "Mr",
-      first: "Ross",
-      last: "Peters",
-    },
-    email: "ross.peters@example.com",
-    picture: {
-      large: "https://randomuser.me/api/portraits/men/29.jpg",
-      medium: "https://randomuser.me/api/portraits/med/men/29.jpg",
-      thumbnail: "https://randomuser.me/api/portraits/thumb/men/29.jpg",
-    },
-    nat: "AU",
-  },
-];
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
+import Meta from "antd/es/card/Meta";
+import { Discover } from "..";
+const SubComponent = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  return (
+    <div>
+      <h2>Discover</h2>
+      {/* <Timeline
+        items={}
+      /> */}
+    </div>
+  );
+};
 const HomePanel: FC<HomePanelProptype> = (props) => {
   return (
     <Flex
       align="start"
-      gap={8}
+      gap={32}
       justify="space-between"
       wrap
       className={styles.panel_container}
     >
-      <Flex vertical className={styles.left_container}>
-        <h2>
-          <BankOutlined />
-          <span className={styles.greet_school}>Welcome to School</span>
-        </h2>
-        <p className={styles.school_message}>{schoolMessage}</p>
-        <h3>
-          <ReconciliationOutlined />
-          <span className={styles.greet_school}>Welcome to School</span>
-        </h3>
-        <p className={styles.school_message}>{schoolMessage}</p>
+      <Flex gap={18} vertical className={styles.left_container}>
+        <Flex vertical>
+          <h2>
+            <BankOutlined />
+            <span className={styles.greet_school}>Welcome to School</span>
+          </h2>
+          <p className={styles.school_message}>{schoolMessage}</p>
+        </Flex>
+        <Discover />
       </Flex>
       <section className={styles.right_container}>
         <h2 className={styles.notification_heading}>Notification</h2>
--- app/component/index.ts
+++ app/component/index.ts
@@ -2,3 +2,4 @@ export { default as NavigationHeader } from "./Navigation";
 export { default as CCard } from "./Card";
 export { default as Announcment } from "./announcement";
 export { default as HomePanel } from "./HomePanel";
+export { default as Discover } from "./Discover";
